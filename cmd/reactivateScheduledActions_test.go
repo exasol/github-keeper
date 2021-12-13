1c9021855702c5170d809cb13eba33c46115718c
Jakob Braun <jakob.braun@posteo.de>
2021-12-13 07:42:08 -0400

    improved coverage

--- cmd/reactivateScheduledActions_test.go
+++ cmd/reactivateScheduledActions_test.go
@@ -1,13 +1,28 @@
 package cmd
 
 import (
+	"github.com/stretchr/testify/suite"
 	"testing"
 )
+
+type ReEnableWorkflowsSuite struct {
+	suite.Suite
+}
+
+func TestReEnableWorkflowsSuite(t *testing.T) {
+	suite.Run(t, new(ReEnableWorkflowsSuite))
+}
 
 /**
   This is a smoke test that only checks that the program does not panic.
 */
-func Test_reEnableWorkflows(t *testing.T) {
+func (suite *ReEnableWorkflowsSuite) Test_reEnableWorkflows() {
+	reactivateScheduledActionsCmd.Run(reactivateScheduledActionsCmd, []string{"testing-release-robot"})
+}
+
+func (suite *ReEnableWorkflowsSuite) TestUnknownRepo() {
 	client := getGithubClient()
-	reEnableWorkflows("testing-release-robot", client)
+	suite.PanicsWithValue("Failed to list the workflows of um-unknown-repo. Cause: GET https://api.github.com/repos/exasol/um-unknown-repo/actions/workflows?per_page=1000: 404 Not Found []", func() {
+		reEnableWorkflows("um-unknown-repo", client)
+	})
 }
