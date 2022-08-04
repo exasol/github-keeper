9b072d52b116f5f26b3f6c892192dc578efa875e
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-08-04 17:25:12 +0200

    Fix linter warnings

--- cmd/secrets_test.go
+++ cmd/secrets_test.go
@@ -13,7 +13,7 @@ func TestSecretsSuite(t *testing.T) {
 	suite.Run(t, new(SecretsSuite))
 }
 
-func (suite SecretsSuite) TestRead() {
+func (suite *SecretsSuite) TestRead() {
 	secrets := ReadSecretsFromYaml("../test_resources/secrets.yml")
 	suite.Assert().Equal("https://slack.com/123", secrets.resolveSecret("issuesSlackWebhookUrl"))
 }
